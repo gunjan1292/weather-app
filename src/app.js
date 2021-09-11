97c858880c7fca124a0875f566ce789979d6aca8
Gunjan Bhardwaj <b92gunjan@gmail.com>
2021-09-12 01:17:17 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const Geo = require("./Utility/geo");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // Define Paths forexpress config
 const publicDirectory = path.join(__dirname, "../public");
